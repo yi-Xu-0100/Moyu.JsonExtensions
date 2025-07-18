cc9c4dcd744dc5a7ee11d9168eb6bc2d195eb1d1
yi_Xu <yi_Xu@yixuju.cn>
2025-07-18 16:08:14 +0800

    feat: 补充ToJsonNode的函数

--- STJ/JsonExtensions.cs
+++ STJ/JsonExtensions.cs
@@ -1,4 +1,5 @@
 using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace Moyu.JsonExtensions.STJ;
 
@@ -53,5 +54,28 @@ public static class JsonExtensions
     {
         var options = JsonOptionFactory.Create(optionType);
         return JsonSerializer.Deserialize<T>(json, options);
+    }
+
+    /// <summary>
+    /// 将对象序列化为 <see cref="JsonNode" />，使用指定的 <see cref="JsonSerializerOptions" />。
+    /// </summary>
+    /// <param name="obj">要序列化的对象。</param>
+    /// <param name="options">JSON 序列化选项。</param>
+    /// <returns>表示 JSON 数据结构的 <see cref="JsonNode" />，如果对象为 null，则返回 null。</returns>
+    public static JsonNode? ToJsonNode(this object obj, JsonSerializerOptions options)
+    {
+        return JsonSerializer.SerializeToNode(obj, options);
+    }
+
+    /// <summary>
+    /// 将对象序列化为 <see cref="JsonNode" />，使用预设的 <see cref="JsonOptionType" /> 枚举选项。
+    /// </summary>
+    /// <param name="obj">要序列化的对象。</param>
+    /// <param name="optionType">预设的 JSON 选项类型，默认为 <see cref="JsonOptionType.EncEnumStrFields"/>。</param>
+    /// <returns>表示 JSON 数据结构的 <see cref="JsonNode" />，如果对象为 null，则返回 null。</returns>
+    public static JsonNode? ToJsonNode(this object obj, JsonOptionType optionType = JsonOptionType.EncEnumStrFields)
+    {
+        var options = JsonOptionFactory.Create(optionType);
+        return JsonSerializer.SerializeToNode(obj, options);
     }
 }
